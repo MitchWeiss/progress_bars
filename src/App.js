068bfd6f1fa979661e01b2df6ded49f8ca220899
Mitchell Weiss <mitch@mitchellweiss.com>
2017-10-26 17:37:58 +1100

    Fetch data from endpoint and render main app

--- src/App.js
+++ src/App.js
@@ -3,15 +3,58 @@ import ProgressBar from './ProgressBar';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {bars: [], buttons: [], loading: true, selected: 0};
+  }
+  componentDidMount() {
+    fetch('http://frontend-exercise.apps.staging.digital.gov.au/bars')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({...data, loading: false});
+    });
+  }
+
+  handleButtonClick(buttonVal) {
+    let { bars, selected } = this.state;
+    bars[selected] = Math.max(bars[selected] + buttonVal, 0)
+    this.setState({bars})
+  }
+
+  buildSelect() {
+    if (this.state.bars.length === 0) return;
+
+    return (
+      <select className="selector"
+        onChange={(e) => this.setState({selected: parseInt(e.target.value, 10)})} 
+        value={this.state.selected}>
+        {this.state.bars.map((bar, i) => {
+          return <option value={i} key={i}>#progress{i+1}</option>
+        })}
+      </select>
+    );
+  }
+
+  buildButtons() {
+    return this.state.buttons.map((val, i) => {
+      return <button key={i} className="btn" onClick={() => {this.handleButtonClick(val)}}>{val}</button>
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <ProgressBar progress={30} />
-        <ProgressBar progress={90} />
-        <ProgressBar progress={0} />
-        <ProgressBar progress={100} />
-        <ProgressBar progress={120} />
-        <ProgressBar progress={-20} />
+      <div className="app">
+        { this.state.loading && <p>Loading...</p> }
+
+        { this.state.bars.map((val, i) => {
+          return <ProgressBar progress={val} key={i} /> //array index only used as key as array does not reorder. Otherwise its dangerous!
+        }) }
+
+        <div className="controls">
+          { this.buildSelect() }
+          { this.buildButtons() }
+        </div>
+
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,76 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import ProgressBar from './ProgressBar';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+let wrapper;
+
+describe('<App>', () => {
+  beforeEach(() => {
+    // Mock fetch to return empty data
+    const p = Promise.resolve({json: ()=>{return {}}});
+    global.fetch = jest.fn().mockImplementation(() => p)
+  })
+
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+
+  it('shows the loading dialog', () => {
+    wrapper = shallow(<App />);
+    expect(wrapper.find('p').text()).toEqual("Loading...");
+  });
+
+  it("doesn't show the selector", () => {
+    wrapper = shallow(<App />);
+    expect(wrapper.find('select').exists()).toBe(false);
+  });
+
+  it("doesn't show any buttons", () => {
+    wrapper = shallow(<App />);
+    expect(wrapper.find('.btn').length).toEqual(0);
+  });
+
+  describe('after fetching data', () => {
+    // Mock fetch to return test data
+    beforeEach(async () => {
+      const testData = { json: function() {
+        return {
+          buttons: [25, 52, -41, -23],
+          bars: [21, 55, 69]
+        }
+      }}
+      const p = Promise.resolve(testData)
+      global.fetch = jest.fn().mockImplementation(() => p)
+      wrapper = shallow(<App />);
+
+      await p.then(); // then() chains after the .json() async call
+      await wrapper.update(); // wait for re-render after adding fetched data to state
+    });
+
+    it('shows the correct number of progress bars', () => {
+      expect(wrapper.find('ProgressBar').length).toEqual(3);
+    }); 
+
+    it("shows the correct number of options in the selector", () => {
+      expect(wrapper.find('option').length).toEqual(3);
+    });
+
+    it("shows the correct number of buttons", () => {
+      expect(wrapper.find('.btn').length).toEqual(4);
+    });
+
+    it("updates the first progress bar", () => {
+      wrapper.find('.btn').first().simulate('click');
+      const newVals = wrapper.find('ProgressBar').map(node => node.props().progress);
+      expect(newVals).toEqual([46, 55, 69]);
+    })
+
+    it("updates the selected progress bar", () => {
+      wrapper.setState({selected: 2});
+      wrapper.find('.btn').first().simulate('click');
+      const newVals = wrapper.find('ProgressBar').map(node => node.props().progress);
+      expect(newVals).toEqual([21, 55, 94]);
+    });
+  });
 });
